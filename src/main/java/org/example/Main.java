e877aacc3ac977b11e16e59cdb2ec5d7a253bf3a
stfilimonova <stfilimonova@mail.ru>
2023-03-18 05:25:03 +0300

    Main modified + CalcServ added + Tests added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         BonusService service = new BonusService();
 
-        long price = 12_000;
+        long price = -12_000;
         boolean registered = true;
 
         long bonus = service.calculate(price, registered);
--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -28,4 +28,85 @@ public class BonusServiceTest {
 
             assertEquals(expected, actual);
         }
+
+        @org.junit.jupiter.api.Test
+        void shouldCalculateForUnregisteredAndUnderLimit() {
+            BonusService service = new BonusService();
+
+            long price = 12_000;
+            boolean registered = false;
+            long expected = 120;
+
+            long actual = service.calculate(price, registered);
+
+            assertEquals(expected, actual);
+        }
+
+        @org.junit.jupiter.api.Test
+        void shouldCalculateForUnregisteredAndOverimit() {
+            BonusService service = new BonusService();
+
+            long price = 100_000;
+            boolean registered = false;
+            long expected = 500;
+
+            long actual = service.calculate(price, registered);
+
+            assertEquals(expected, actual);
+        }
+
+        @org.junit.jupiter.api.Test
+        void bonusBoundaryValueOver500() {
+            BonusService service = new BonusService();
+
+            long price = 16_700;
+            boolean registered = true;
+            long expected = 500;
+
+            long actual = service.calculate(price, registered);
+
+            assertEquals(expected, actual);
+        }
+
+        @org.junit.jupiter.api.Test
+        void bonusBoundaryValue500() {
+            BonusService service = new BonusService();
+
+            long price = 16_667;
+            boolean registered = true;
+            long expected = 500;
+
+            long actual = service.calculate(price, registered);
+
+            assertEquals(expected, actual);
+        }
+
+        @org.junit.jupiter.api.Test
+        void bonusBoundaryValueUnder500() {
+            BonusService service = new BonusService();
+
+            long price = 49_960;
+            boolean registered = false;
+            long expected = 499;
+
+            long actual = service.calculate(price, registered);
+
+            assertEquals(expected, actual);
+        }
+
+        @org.junit.jupiter.api.Test
+        void negativeCost() {
+            BonusService service = new BonusService();
+
+            long price = -13_500;
+            boolean registered = false;
+            long expected = 0;
+
+            long actual = service.calculate(price, registered);
+
+            assertEquals(expected, actual);
+    }
+    // последний тест провален, в коде калькулятора не предусмотрена
+    // ситуация ввода невалидного (отрицательного) значения в поле стоимости
+
     }
